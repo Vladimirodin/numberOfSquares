b60ed1bfa3c07ada7fd21b6b3a2c1a23448f99d0
Vladimir Odintcev <vovaodintcev@gmail.com>
2022-09-20 20:38:49 +0500

    modified main test

--- src/main/java/ru/netology/sqr/numberOfSquares/services/SQRService.java
+++ src/main/java/ru/netology/sqr/numberOfSquares/services/SQRService.java
@@ -3,11 +3,12 @@ package ru.netology.sqr.numberOfSquares.services;
 public class SQRService {
 
     public int numberOfSquares(int beginningRange, int endRange) {
+        int quantity = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= beginningRange && i * i <= endRange) {
-                return i;
+                quantity++;
             }
         }
-        return -1;
+        return quantity;
     }
 }
